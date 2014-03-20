20a2e374659763352e0243ad2ce682253105aadb
Reika <reikasminecraft@gmail.com>
2014-03-20 09:59:01 -0400

    Update

--- OldMeteorGenerator.java
+++ OldMeteorGenerator.java
@@ -3,7 +3,10 @@ package Reika.MeteorCraft;
 import java.util.Random;
 
 import net.minecraft.world.World;
+import net.minecraft.world.WorldType;
+import net.minecraft.world.biome.BiomeGenBase;
 import net.minecraft.world.chunk.IChunkProvider;
+import Reika.DragonAPI.ModInteract.ReikaTwilightHelper;
 import Reika.MeteorCraft.MeteorGenerator.MeteorType;
 import Reika.MeteorCraft.Registry.MeteorOptions;
 import cpw.mods.fml.common.IWorldGenerator;
@@ -24,22 +27,37 @@ public class OldMeteorGenerator implements IWorldGenerator {
 		int id = world.provider.dimensionId;
 		if (id == -1 || id == 1 || world.provider.hasNoSky)
 			return false;
+		if (world.getWorldInfo().getTerrainType() == WorldType.FLAT)
+			return false;
 		return true;
 	}
 
 	private void generate(Random rand, World world, int chunkX, int chunkZ) {
 		int x = chunkX+rand.nextInt(16);
 		int z = chunkZ+rand.nextInt(16);
-		int y = this.getGenY(world, x, z);
-		MeteorGenerator.instance.generate(world, x, y, z, this.getRandomType());
+		int y = this.getGenY(world, x, z, rand);
+		MeteorGenerator.instance.generate(world, x, y, z, this.getRandomType(rand));
 	}
 
-	private MeteorType getRandomType() {
-		return null;
+	private MeteorType getRandomType(Random rand) {
+		if (rand.nextInt(10) == 0)
+			return MeteorType.END;
+		else if (rand.nextInt(5) == 0)
+			return MeteorType.NETHERRACK;
+		else
+			return MeteorType.STONE;
 	}
 
-	private int getGenY(World world, int x, int z) {
-		return 0;
+	private int getGenY(World world, int x, int z, Random rand) {
+		if (world.provider.dimensionId == ReikaTwilightHelper.getDimensionID())
+			return 5+rand.nextInt(30);
+		BiomeGenBase biome = world.getBiomeGenForCoords(x, z);
+		int max = (int)(biome.maxHeight*world.provider.getAverageGroundLevel());
+		int y = 5+rand.nextInt(max);
+		while (world.canBlockSeeTheSky(x, y+1, z)) {
+			y = 5+rand.nextInt(max);
+		}
+		return y;
 	}
 
 }
