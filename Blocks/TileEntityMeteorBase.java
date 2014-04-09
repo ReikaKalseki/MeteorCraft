0f7c0cd8816d51c140b074e1e005f2ad489d3e52
Reika <reikasminecraft@gmail.com>
2014-04-09 14:02:56 -0400

    Update

--- Blocks/TileEntityMeteorBase.java
+++ Blocks/TileEntityMeteorBase.java
@@ -48,7 +48,7 @@ public abstract class TileEntityMeteorBase extends TileEntityBase implements Sha
 	}
 
 	@Override
-	public void animateWithTick(World world, int x, int y, int z) {
+	protected void animateWithTick(World world, int x, int y, int z) {
 		if (iotick > 0)
 			iotick -= 8;
 	}
