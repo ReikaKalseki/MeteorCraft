7710560f7bebb223e8833d422215e83cee91acdc
Reika <reikasminecraft@gmail.com>
2014-12-19 02:42:39 -0500

    Update

--- Registry/MeteorSounds.java
+++ Registry/MeteorSounds.java
@@ -12,6 +12,7 @@ package Reika.MeteorCraft.Registry;
 import java.net.URL;
 
 import net.minecraft.client.audio.SoundCategory;
+import net.minecraft.entity.Entity;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.world.World;
 import Reika.DragonAPI.Instantiable.Data.WorldLocation;
@@ -50,6 +51,14 @@ public enum MeteorSounds implements SoundEnum {
 		}
 		name = n;
 		path = PREFIX+SOUND_FOLDER+name+SOUND_EXT;
+	}
+
+	public void playSound(Entity e) {
+		this.playSound(e, 1, 1);
+	}
+
+	public void playSound(Entity e, float vol, float pitch) {
+		this.playSound(e.worldObj, e.posX, e.posY, e.posZ, vol, pitch);
 	}
 
 	public void playSound(World world, double x, double y, double z, float vol, float pitch) {
